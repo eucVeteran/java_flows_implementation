3064f878b1f0c78543769caccecba3c85de188e8
Azizbek Toshpulatov <xtoshpul@fi.muni.cz>
2024-05-09 10:25:35 +0200

    Added JavaDoc :D

--- src/main/java/cz/muni/fi/pb162/hw02/impl/FlowSourceDecorator.java
+++ src/main/java/cz/muni/fi/pb162/hw02/impl/FlowSourceDecorator.java
@@ -5,11 +5,16 @@ import cz.muni.fi.pb162.hw02.FlowSource;
 /**
  * Basic decorator of {@link FlowSource}, that has basic advance and get methods.
  *
+ * @param <T> the type of elements in the source {@link FlowSource}.
  * @author Azizbek Toshpulatov
  */
 public abstract class FlowSourceDecorator<T> implements FlowSource<T> {
     private final FlowSource<T> source;
 
+    /**
+     * Basic constructor that sets the source.
+     * @param source source of elements.
+     */
     public FlowSourceDecorator(FlowSource<T> source) {
         this.source = source;
     }
